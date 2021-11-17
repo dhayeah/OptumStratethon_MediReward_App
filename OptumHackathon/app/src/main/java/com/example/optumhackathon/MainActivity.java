dedf7a8ee3f2b7c5aaf34aced5f65bfd9976a8cd
dhayeah <57786651+dhayeah@users.noreply.github.com>
2021-11-17 20:54:42 +0530

    Update MainActivity.java

--- OptumHackathon/app/src/main/java/com/example/optumhackathon/MainActivity.java
+++ OptumHackathon/app/src/main/java/com/example/optumhackathon/MainActivity.java
@@ -20,19 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        Calendar calendar= Calendar.getInstance();
-//        calendar.set(Calendar.HOUR_OF_DAY,18);
-//        calendar.set(Calendar.MINUTE,51);
-//        calendar.set(Calendar.SECOND,0);
-//
-//        Intent intent =new Intent(getApplicationContext(),Notification_reciever.class);
-//        intent.setAction("hELLO");
-//        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent=PendingIntent.getBroadcast(getApplicationContext(),100,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-//        AlarmManager alarmManager=(AlarmManager) getSystemService(ALARM_SERVICE);
-//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
-
-
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 19);
